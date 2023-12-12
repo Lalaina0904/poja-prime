d2764c9ca6291cd2f876e9b2baf03fb8bde62973
Lalaina0904 <lalainap19.aps2b@gmail.com>
2023-12-12 11:22:54 +0300

    implement metod to generate prime numbers

--- src/main/java/com/hei/course/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/hei/course/endpoint/rest/controller/PrimeController.java
@@ -1,28 +1,16 @@
-//package com.hei.course.endpoint.rest.controller;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.math.BigInteger;
-//import java.security.SecureRandom;
-//
-//@RestController
-//public class PrimeController {
-//    @GetMapping("/prime")
-//    public static BigInteger generatePrime(int bitLength) {
-//        SecureRandom random = new SecureRandom();
-//        BigInteger prime;
-//
-//        BigInteger probablePrime = new BigInteger(bitLength, random);
-//
-//        while (true) {
-//            if (probablePrime.isProbablePrime(1)) {
-//                prime = probablePrime;
-//                break;
-//            }
-//            probablePrime = new BigInteger(bitLength, random);
-//        }
-//
-//        return prime;
-//    }
-//}
+package com.hei.course.endpoint.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+@RestController
+public class PrimeController {
+    @GetMapping("/prime")
+    public static BigInteger generatePrime() {
+        BigInteger prime = BigInteger.probablePrime(10000, new SecureRandom());
+        return prime;
+    }
+}
