0dc5d5cb391dd54f6be8fc1a219b1eaa756f8a36
Lalaina0904 <lalainap19.aps2b@gmail.com>
2023-12-12 19:20:23 +0300

    🐛

--- src/test/java/com/hei/course/endpoint/rest/controller/PrimeControllerIT.java
+++ src/test/java/com/hei/course/endpoint/rest/controller/PrimeControllerIT.java
@@ -1,27 +1,24 @@
 package com.hei.course.endpoint.rest.controller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@RestController
+
 public class PrimeControllerIT {
 
-    @Autowired
-    private PrimeController controller;
-
     @Test
-    public void testGeneratePrime() {
+    public void PrimeControllerTest() {
+        PrimeController controller = new PrimeController();
+
         BigInteger prime = controller.generatePrime();
 
-        assertTrue("The number is not probable prime", prime.isProbablePrime(99));
+        assertTrue("The number is not probable prime", prime.isProbablePrime(100));
 
-        assertEquals("The number length is not 10000 bits", 10_000, prime.bitLength());
+        assertEquals("The number length is not 100 bits", 100, prime.bitLength());
     }
 
 }
