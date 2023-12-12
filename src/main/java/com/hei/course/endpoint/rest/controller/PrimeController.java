3507f443aa0b8f399a82f94914f2fc0c4beff170
Lalaina0904 <lalainap19.aps2b@gmail.com>
2023-12-12 11:56:24 +0300

    fix : resolve bugs and add test

--- src/main/java/com/hei/course/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/hei/course/endpoint/rest/controller/PrimeController.java
@@ -9,8 +9,8 @@ import java.security.SecureRandom;
 @RestController
 public class PrimeController {
     @GetMapping("/prime")
-    public static BigInteger generatePrime() {
-        BigInteger prime = BigInteger.probablePrime(10000, new SecureRandom());
-        return prime;
+    public BigInteger generatePrime() {
+        SecureRandom random = new SecureRandom();
+        return BigInteger.probablePrime(10000, random);
     }
 }
--- src/test/java/com/hei/course/endpoint/rest/controller/PrimeControllerIT.java
+++ src/test/java/com/hei/course/endpoint/rest/controller/PrimeControllerIT.java
@@ -1,21 +1,27 @@
-//package com.hei.course.endpoint.rest.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.math.BigInteger;
-//@RestController
-//public class PrimeControllerIT {
-//    private final PrimeService primeService;
-//
-//    @Autowired
-//    public PrimeController(PrimeService primeService) {
-//        this.primeService = primeService;
-//    }
-//
-//    @GetMapping("/generatePrime")
-//    public BigInteger generatePrime() {
-//        return primeService.generateRandomPrime();
-//    }
-//}
+package com.hei.course.endpoint.rest.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RestController
+public class PrimeControllerIT {
+// test code
+@Test
+public void testGeneratePrime() {
+    PrimeController controller = new PrimeController();
+    BigInteger prime = controller.generatePrime();
+
+    assertTrue("The number is not probable prime", prime.isProbablePrime(99));
+
+    assertEquals("The number length is not 10000 bits", 10_000, prime.bitLength());
+}
+
+}
