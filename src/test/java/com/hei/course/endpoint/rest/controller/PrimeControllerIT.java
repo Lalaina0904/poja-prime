c92b28d3735c2d5e502f01f5c9233777a97d20f2
Lalaina0904 <lalainap19.aps2b@gmail.com>
2023-12-12 18:26:01 +0300

    fix : "change test for 10 bits length"

--- src/test/java/com/hei/course/endpoint/rest/controller/PrimeControllerIT.java
+++ src/test/java/com/hei/course/endpoint/rest/controller/PrimeControllerIT.java
@@ -13,15 +13,17 @@ import static org.junit.Assert.assertTrue;
 
 @RestController
 public class PrimeControllerIT {
-// test code
-@Test
-public void testGeneratePrime() {
-    PrimeController controller = new PrimeController();
-    BigInteger prime = controller.generatePrime();
 
-    assertTrue("The number is not probable prime", prime.isProbablePrime(99));
+    @Autowired
+    private PrimeController controller;
 
-    assertEquals("The number length is not 10000 bits", 10_000, prime.bitLength());
-}
+    @Test
+    public void testGeneratePrime() {
+        BigInteger prime = controller.generatePrime();
+
+        assertTrue("The number is not probable prime", prime.isProbablePrime(100));
+
+        assertEquals("The number length is not 10 bits", 10, prime.bitLength());
+    }
 
 }
