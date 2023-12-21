b765dc00b5e8963f25436697098c4ffa34a90947
Lalaina0904 <lalainap19.aps2b@gmail.com>
2023-12-21 11:54:02 +0300

    rm : remove github workflows

--- src/main/java/com/hei/course/PojaApplication.java
+++ src/main/java/com/hei/course/PojaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-import java.math.BigInteger;
+// import java.math.BigInteger;
 
 @SpringBootApplication
 @PojaGenerated
